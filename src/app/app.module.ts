13a6d4c0007513f67a35de088478a0138e4043c4
tanatat01 <tanatat253919@gmail.com>
2019-04-06 18:50:09 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,31 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ActivityPage } from '../pages/activity/activity';
+import { NewactivityPage } from '../pages/newactivity/newactivity';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ActivityPage,
+    NewactivityPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ActivityPage,
+    NewactivityPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { ActivityPage } from '../activity/activity';
+import { NewactivityPage } from '../newactivity/newactivity';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  addedToDoList:any[]=[];
+  tempTodos:any='';
+  todoPage = NewactivityPage;
+  constructor(public navCtrl: NavController, public navParams:NavParams, public storage:Storage) {
+    this.storage.get('todoDetails').then((val)=> {this.addedToDoList = val;
+  }); 
+}
+  testObj = {"title":"","description":""};
 
-  constructor(public navCtrl: NavController) {
+  ngOnInit():void{
+    this.storage.get('todoDetails').then((val)=>{this.addedToDoList = val;})
+  };
 
+  ionViewWillEnter(){
+    this.storage.get('todoDetails').then((val)=>{this.addedToDoList = val;})
+  };
+
+  goToAddTodo():void{
+    this.navCtrl.push(NewactivityPage);
   }
 
+  removeItem(todo):void{
+    let index = this.addedToDoList.indexOf(todo);
+      if(index>-1){
+        this.addedToDoList.splice(index,1);
+        this.storage.set('todoDetails',this.addedToDoList);
+    }
+  }
 }
